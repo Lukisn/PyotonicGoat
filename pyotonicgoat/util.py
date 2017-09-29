0a61bb7abdc5163179a660c3e2a9ac3b3118443e
Lukas Reimer <lukas.reimer@icloud.com>
2017-09-29 14:43:17 +0200

    fixed format resetting on default bug in colored terminal output function

--- pyotonicgoat/util.py
+++ pyotonicgoat/util.py
@@ -10,20 +10,30 @@ def now():
     return datetime.now(timezone.utc).astimezone()
 
 
-def colored(text, color, bold=False, bg_color="default"):
+def colored(text, color, background="default", bold=False):
     """Return colored text for output on the terminal."""
     color_codes = {
         "default": 0, "black": 30, "red": 31, "green": 32, "yellow": 33,
         "blue": 34, "magenta": 35, "cyan": 36, "white": 37}
-    bg_color_codes = {
+    background_codes = {
         "default": 0, "black": 40, "red": 41, "green": 42, "yellow": 43,
         "blue": "44", "magenta": 45, "cyan": 46, "white": 47}
-    color_code = color_codes.get(color, "default")
-    bg_color_code = bg_color_codes.get(bg_color, "default")
-    bold_code = "1" if bold else "0"
-    modifier = f"\033[{color_code};{bold_code};{bg_color_code}m"
-    reset = "\033[0;0;0m"
-    return f"{modifier}{text}{reset}"
+    color = color_codes.get(color, "default")
+    background = background_codes.get(background, "default")
+    bold = 1 if bold else 0
+    if not color and not background and not bold:
+        return text
+    else:
+        modifiers = []
+        if color:
+            modifiers.append(f"\033[{color}m")
+        if background:
+            modifiers.append(f"\033[{background}m")
+        if bold:
+            modifiers.append(f"\033[{bold}m")
+        modifier = "".join(modifiers)
+        reset = "\033[0m"
+        return f"{modifier}{text}{reset}"
 
 
 def colored_status(status):
